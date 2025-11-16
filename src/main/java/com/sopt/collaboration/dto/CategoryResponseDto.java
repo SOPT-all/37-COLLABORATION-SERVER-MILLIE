b0c9cdbd51022f0b7aeb1bac7e673a6ca6f79bd0
Seyeon CHO <crownjoe@naver.com>
2025-11-17 01:02:31 +0900

    [Fix] Category: CategoryResponseDto Swagger 추가

--- src/main/java/com/sopt/collaboration/dto/CategoryResponseDto.java
+++ src/main/java/com/sopt/collaboration/dto/CategoryResponseDto.java
@@ -1,15 +1,23 @@
 package com.sopt.collaboration.dto;
 
-
 import com.sopt.collaboration.entity.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(name = "CategoryResponseDto", description = "카테고리 응답 DTO")
 public class CategoryResponseDto {
 
+    @Schema(description = "카테고리 ID", example = "1")
     private Long categoryId;
+
+    @Schema(description = "카테고리 제목", example = "소설")
     private String title;
+
+    @Schema(description = "카테고리 설명", example = "추리/스릴러,킬러 스파이,법의학 스릴러,SF")
     private String description;
+
+    @Schema(description = "카테고리 이미지 URL", example = "s3://millie-collaboration-bucket/images/category-images/CATEGORY-IMAGE-NOVEL.png")
     private String imageUrl;
 
     private CategoryResponseDto(Long categoryId, String title, String description, String imageUrl) {
