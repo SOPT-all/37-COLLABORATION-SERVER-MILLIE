842443593159179c8b3c0d4c38b33adcc699efbb
Kimgyuilli <rlarbdlf222@gmail.com>
2025-11-17 16:05:34 +0900

    [Refactor] global: S3 패키지 구조 일관성 있게 수정

--- src/main/java/com/sopt/collaboration/global/s3/service/S3Service.java
+++ src/main/java/com/sopt/collaboration/global/s3/service/S3Service.java
@@ -1,4 +1,4 @@
-package com.sopt.collaboration.global.service;
+package com.sopt.collaboration.global.s3.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
