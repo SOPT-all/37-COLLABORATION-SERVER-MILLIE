5766f8286e36bf34e3038ca8bc63cc35fda734d1
Seyeon CHO <crownjoe@naver.com>
2025-11-17 16:32:43 +0900

    [Fix] Category: CategoryService 의존성 주입 수정

--- src/main/java/com/sopt/collaboration/service/CategoryService.java
+++ src/main/java/com/sopt/collaboration/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.sopt.collaboration.entity.Category;
 import com.sopt.collaboration.global.exception.CategoryException;
 import com.sopt.collaboration.global.response.error.CategoryErrorCode;
 import com.sopt.collaboration.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +12,10 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
-
-    public CategoryService(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
 
     public List<Category> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
@@ -28,5 +26,4 @@ public class CategoryService {
 
         return categories;
     }
-
 }
