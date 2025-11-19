708af6ba3dfeb930d1f105760635e4e735959d92
Seyeon CHO <crownjoe@naver.com>
2025-11-19 12:14:20 +0900

    [Fix]: Banner: 더미 데이터 s3에 맞는 이미지 경로 수정

--- db-migration/insert-banner-data.sql
+++ db-migration/insert-banner-data.sql
@@ -7,4 +7,4 @@ SET character_set_results = utf8mb4;
 
 INSERT INTO Banner (title, content, banner_image_key, book_id)
 VALUES
-    ('《홍학의 자리》 읽을 준비!', '이 책부터 읽어야 재미가 2배', 'BANNER_IMAGE_1', 1);
+    ('《홍학의 자리》 읽을 준비!', '이 책부터 읽어야 재미가 2배', 'images/banners/MAIN_BANNER.png', 1);
